b8199c19ae9fb5c19a856390e69829ea9596bc17
Agatha <Agathaalmunir@gmail.com>
2021-03-20 07:17:42 -0400

    connect js to index and edit panagram js

--- Grocery-list-scripts.js
+++ Grocery-list-scripts.js
@@ -16,19 +16,19 @@ ${secondItemAmount} ${secondItem}
 ${thirdItemAmount} ${thirdItem}
 Total number of items: ${totalAmount}`);
 
-// Part 2
+Part 2
 let tongueTwister = "How much wood would a woodchuck chuck, if the woodchuck could chuck wood?";
 let newTwister = tongueTwister.toLowerCase();
 console.log(newTwister);
 
 
-let pangram = "The quick brown fox jumps over the lazy dog.";
-pangram.indexOf("fox");
-console.log(pangram.indexOf("fox").toCapitalize;)
+let panagram = "The quick brown fox jumps over the lazy dog.";
+let index = panagram.indexOf("fox");
+console.log(panagram.charAt(index).toUpperCase());
 
 
-let anotherPangram = "The five boxing wizards jump quickly.";
-console.log(anotherPangram[10]);
+let anotherPanagram = "The five boxing wizards jump quickly.";
+console.log(anotherPanagram[10]);
 
 
 
